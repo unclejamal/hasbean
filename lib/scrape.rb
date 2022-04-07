47d80f4f7c815e1ce60f3a36dab00dfd17c0357c
unclejamal <unclejamal@gmail.com>
2022-04-07 21:46:44 +0100

    Adapt to the new website (paging does not work still)

--- lib/scrape.rb
+++ lib/scrape.rb
@@ -42,49 +42,48 @@ class HasBeanProductPage
   end
 
   def extract_name
-    find("div.product-single h1[itemprop='name']").text
+    find("div.product-container h1").text
   end
 
   def extract_notes
-    find("div.product-single p.h2").text
+    notes = all("div.product-container h4").first
+    notes ? notes.text.strip : "n/a"
   end
 
   def extract_price
-    find("div.product-single span#ProductPrice").text
+    price = all("div.product-container span.price-item").first
+    price ? price.text.strip : "n/a"
   end
 
   def extract_score
-    total = all("div#cupping-notes li", text: /Total.*\(max. 100\).*:.*/).first
-    return total.text.scan(/Total.*\(max. 100\).*: (.*)/)[0][0] if total
-
-    total = all("div#cupping-notes p", text: /Total.*\(max. 100\).*:.*/).first
-    return total.text.scan(/Total.*\(max. 100\).*: (.*)/)[0][0] if total
+    total = all("div.product-container div#product_accordion_1 li", text: /Total.*\(max.? 100\).*:.*/).first
+    return total.text.scan(/Total.*\(max.? 100\).*: (.*)/)[0][0] if total
 
     return "n/a"
   end
 
   def extract_roast
-    roast = all("div#cupping-notes p", text: /Roast.*/).first
+    roast = all("div.product-container div#product_accordion_1 p", text: /Roast.*/).first
     roast ? roast.text.scan(/Roast.*Information(.*)/)[0][0] : "n/a"
   end
 
   def extract_country
-    country = all("div#details li", text: /Country:.*/).first
+    country = all("div.product-container li.accordion li", text: /Country:.*/).first
     country ? country.text.scan(/Country:(.*)/)[0][0].strip : "n/a"
   end
 
   def extract_province
-    province = all("div#details li", text: /(Province|Region):.*/).first
+    province = all("div.product-container li.accordion li", text: /(Province|Region):.*/).first
     province ? province.text.scan(/(Province|Region):(.*)/)[0][1].strip : "n/a"
   end
 
   def extract_farm
-    farm = all("div#details li", text: /(Farm|Farm name):.*/).first
+    farm = all("div.product-container li.accordion li", text: /(Farm|Farm name):.*/).first
     farm ? farm.text.scan(/(Farm|Farm name):(.*)/)[0][1].strip : "n/a"
   end
 
   def extract_process
-    process = all("div#details li", text: /(Process|Processing|Processing method):.*/).first
+    process = all("div.product-container li.accordion  li", text: /(Process|Processing|Processing method):.*/).first
     process ? process.text.scan(/(Process|Processing|Processing method):(.*)/)[0][1].strip : "n/a"
   end
 end
@@ -101,7 +100,13 @@ class HasBeanCoffeeCollectionPage
     puts "PAWEL Scraping Collection Page"
     visit "https://www.hasbean.co.uk/collections/coffee"
 
-    coffees=all('.grid-link').to_a
+    while true do
+      button = all("button.btn", text: /Load More/).first
+      break if !button
+      button.click
+    end
+
+    coffees=all('li a.btn').to_a
     coffee_links=coffees.map { |c| c['href'] }.take(@limit)
 
     return coffee_links.map { |cl| HasBeanProductPage.new(cl).scrape }
